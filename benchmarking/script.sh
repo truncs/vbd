91781470f71ad7e7568b488c614e92ac62211649
truncs <sarawgi.aditya@gmail.com>
2011-12-11 01:33:20 -0500

    New benchmarking data

--- benchmarking/script.sh
+++ benchmarking/script.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
-mkfs.ext2 /dev/vbd
-mount /dev/vbd /mnt
+insmod ../vbd.ko read_latency=20 write_latency=20 nsectors=1048576
+mkfs.btrfs  /dev/vbd
+mount  /dev/vbd /mnt
 mount
-iozone -a -s 128 /mnt > ext2_12_12
+cd /mnt
+iozone -I -s 10K -r 1K > /home/sanket/vbd/new/btrfs_20_20
+cd -
 umount /mnt
-mkfs.ext3 /dev/vbd
-mount /dev/vbd /mnt
-mount
-iozone -a -s 128 /mnt > ext3_12_12
-umount /mnt
-mkfs.ext4 /dev/vbd
-mount /dev/vbd /mnt
-mount
-iozone -a -s 128 /mnt > ext4_12_12
-umount /mnt
-mkfs.xfs /dev/vbd
-mount /dev/vbd /mnt
-mount
-iozone -a -s 128 /mnt > xfs_12_12
-umount /mnt
-mkfs.btrfs /dev/vbd
-mount /dev/vbd /mnt
-mount
-iozone -a -s 128 /mnt > btrfs_12_12
-umount /mnt
-mkfs.reiserfs -f /dev/vbd
-mount /dev/vbd /mnt
-mount
-iozone -a -s 128 > reiser_12_12
-umount /mnt
-cat /proc/vbd > stats_12_12
+cat /proc/vbd > /home/sanket/vbd/new/stats_btrfs_20_20
 rmmod vbd.ko
+
+insmod ../vbd.ko read_latency=15 write_latency=20  nsectors=1048576
+mkfs.btrfs  /dev/vbd
+mount  /dev/vbd /mnt
+cd /mnt
+iozone -I -s 10K -r 1K > /home/sanket/vbd/new/btrfs_15_20
+cd -
+umount /mnt
+cat /proc/vbd > /home/sanket/vbd/new/stats_btrfs_15_20
+rmmod vbd.ko
+
+insmod ../vbd.ko read_latency=10 write_latency=20 nsectors=1048576
+mkfs.btrfs  /dev/vbd
+mount  /dev/vbd /mnt
+cd /mnt
+iozone -I -s 10K -r 1K > /home/sanket/vbd/new/btrfs_10_20
+cd -
+umount /mnt
+cat /proc/vbd > /home/sanket/vbd/new/stats_btrfs_10_20
+rmmod vbd.ko
+
+
+
+
